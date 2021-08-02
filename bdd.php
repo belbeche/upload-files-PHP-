8905077ac261d392d801ec1767b807e1a1a711e8
= <wbelbeche.s@gmail.com>
2021-08-02 10:26:57 +0200

    pause projet

---
+++ bdd.php
@@ -0,0 +1,6 @@
+<?php
+try {
+    $db = new PDO('mysql:host=localhost;dbname=upload_file', 'root', '');
+} catch (PDOException $e) {
+    die('Erreur connexion: ' . $e->getMessage());
+}
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require './bdd.php';
 // une fois le formulaire envoyé , on récupère ces valeurs sinon erreur index undefined
 if (isset($_FILES['file'])) {
     $tmpName = $_FILES['file']['tmp_name'];
@@ -10,7 +11,7 @@ if (isset($_FILES['file'])) {
     // la fonction strtolower permets de mettre en minuscule tout une chaîne (ex: jpg, JPG , jpG)
     $extension = strtolower(end($tabExtension));
 
-    $extensions = ['jpg', 'png', 'jpeg', 'gif'];
+    $extensions = ['jpg', 'png', 'jpeg', 'gif', 'jfif'];
     // Taille max que l'on accepte
     $maxSize = 400000;
     // on vérfie bien que la taille du fichier est inférieur à maxSize
@@ -18,7 +19,14 @@ if (isset($_FILES['file'])) {
         //uniqid génère une chaine de caractère uniq 5f586bf96dcd38.73540086
         $uniqueName = uniqid('', true);
         //$file = 5f586bf96dcd38.73540086.jpg
-        $file = $uniqName . "." . $extension;
+        $file = $uniqueName . "." . $extension;
+        // on fait une requete preparer pour éviter les injection SQL , et enregistrer le nom du fichier en BDD
+        $req = $db->prepare('INSERT INTO file (name) VALUES (?)');
+        /**
+         * La fonction execute() attend 1 seul paramètre qui est un tableau. 
+         * Dans ce tableau on doit lui passer autant de paramètre que l’on de « ? » dans la requête.
+         */
+        $req->execute([$file]);
 
         move_uploaded_file($tmpName, './upload/' . $file);
     } else {
@@ -44,6 +52,17 @@ if (isset($_FILES['file'])) {
         <input type="file" name="file">
         <button type="submit">Enregistrer</button>
     </form>
+    <h2>Mes images</h2>
+    <?php
+
+    $req = $db->Query('SELECT name FROM file');
+    /**
+     * Tant qu'il y'a des résultats on continue , le fetch est une méthode PDO.
+     */
+    while ($data = $req->fetch()) {
+        echo "<img src='./upload/" . $data['name'] . "' width='300px' ><br>";
+    }
+    ?>
 
 </body>
 
